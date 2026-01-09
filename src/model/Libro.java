132d36e35d63a17e1cc8da8f9803bc2cc5987d73
gtapiero <giovanny.tapiero@pi.edu.co>
2026-01-09 10:10:36 -0500

    commit - Libro .

--- src/model/Libro.java
+++ src/model/Libro.java
@@ -1,4 +1,49 @@
 package model;
 
 public class Libro {
+
+    private int id;
+    private String titulo;
+    private String autor;
+    private int anio;
+
+    public Libro() {}
+
+    public Libro(String titulo, String autor, int anio) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anio = anio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
 }
