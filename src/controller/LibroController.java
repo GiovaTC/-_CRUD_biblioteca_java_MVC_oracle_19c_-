86dfab7d53c83c278562d1c93d8dbd39360af24e
gtapiero <giovanny.tapiero@pi.edu.co>
2026-01-09 11:14:42 -0500

    commit - LibroController .

--- src/controller/LibroController.java
+++ src/controller/LibroController.java
@@ -1,4 +1,28 @@
 package controller;
 
+import dao.LibroDAO;
+import model.Libro;
+import java.util.List;
+
 public class LibroController {
-}
+
+    private final LibroDAO dao = new LibroDAO();
+
+    public void agregarLibro(String titulo, String autor, int anio) {
+        dao.insertar(new Libro(titulo, autor, anio));
+    }
+
+    public List<Libro> obtenerLibros() {
+        return dao.listar();
+    }
+
+    public void actualizarLibro(int id, String titulo, String autor, int anio) {
+        Libro l = new Libro(titulo, autor, anio);
+        l.setId(id);
+        dao.actualizar(l);
+    }
+
+    public void eliminarLibro(int id) {
+        dao.eliminar(id);
+    }
+}   
